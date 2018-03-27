d52fa9a8bd9177b1a87d86e3585a2c209ec5e378
danieladasilva <36673150+danieladasilva@users.noreply.github.com>
2018-03-27 19:58:41 +0300

    Update MaksukorttiTest.java

--- laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
+++ laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
@@ -8,28 +8,9 @@ import static org.junit.Assert.*;
 public class MaksukorttiTest {
     Maksukortti kortti;
     
-    public MaksukorttiTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
     @Before
     public void setUp() {
         kortti = new Maksukortti(10);
-    }
-    
-    @After
-    public void tearDown() {
-    }
-    
-    @Test
-    public void hello() {
     }        
      
     @Test
